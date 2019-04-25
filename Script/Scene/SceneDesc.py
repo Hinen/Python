5d127e5d8a6f045baaf9a6db225e2128427747c1
Hinen <qkrqldtjqj@naver.com>
2019-04-26 00:51:32 +0900

    Add GoMenuScene in SceneeDesc

--- Script/Scene/SceneDesc.py
+++ Script/Scene/SceneDesc.py
@@ -5,3 +5,9 @@ class SceneDesc(SceneBase):
         super().__init__(type, win, canvas, sceneManager)
 
         self.createText(400, 100, "설명", 40)
+        self.createButton(650, 550, 20, 2, "뒤로가기", 20, self.goMenu)
+
+
+    def goMenu(self):
+        SoundManager.get().playFX("select.wav")
+        self.sceneManager.sceneChange(SceneType.MENU)
