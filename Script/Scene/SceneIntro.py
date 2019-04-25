fbfb7391f231a4a4ec7e32b30ea653e3accf7907
Hinen <qkrqldtjqj@naver.com>
2019-04-26 00:19:53 +0900

    Change bgm dir

--- Script/Scene/SceneIntro.py
+++ Script/Scene/SceneIntro.py
@@ -9,7 +9,7 @@ class SceneIntro(SceneBase):
         self.createText(400, 100, "뇌가 이상해지는 변환기", 40)
         self.registerTimer(2, self.createPressAnyKeyText)
 
-        pygame.mixer.music.load('Resources/Sound/taebo.wav')
+        pygame.mixer.music.load('Resources/Sound/BGM/taebo.wav')
         pygame.mixer.music.play(-1)
 
     def createPressAnyKeyText(self):
