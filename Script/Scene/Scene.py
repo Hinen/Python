cc0dd44153183e173d0f9b98bf360dec2170d1f2
Awesomepiece_Hinen <hinen@awesomepiece.com>
2019-04-26 10:31:14 +0900

    # -*- coding: utf-8 -*-

--- Script/Scene/Scene.py
+++ Script/Scene/Scene.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import enum
 from tkinter import *
 from Script.Utility.SoundManager import *
--- Script/Scene/SceneGame.py
+++ Script/Scene/SceneGame.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from .Scene import *
 import random
 
